682adf15773c1d9927e2be3566cdb7e200e52c65
kurtreyn <kurtreyn@yahoo.com>
2023-07-16 20:39:27 -0400

    vectors

---
+++ src/vectors.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    vector<int> vectorArr = {1,2,3,4,5};
+    cout << "vectorArr: " << vectorArr[0] << endl;
+
+
+    return 0;
+}
