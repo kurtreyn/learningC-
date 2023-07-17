808cfa1575525976f14d863b88599c18333be07f
kurtreyn <kurtreyn@yahoo.com>
2023-07-16 20:11:46 -0400

    added new item

---
+++ src/readWriteFromConsole.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter a value: " << endl;
+    int value;
+    cin >> value;
+    cout << "You entered: " << value << endl;
+
+    return 0;
+}
